6324d9776f7b2914d39060029d13ec177b0c86fa
par-der <egorterentev58@gmail.com>
2023-05-31 15:59:23 +0400

    finish all cart context

--- client/src/components/CategoryNavMobile.js
+++ client/src/components/CategoryNavMobile.js
@@ -1,8 +1,15 @@
 import React from "react";
 // icons
 import { FiX } from "react-icons/fi";
+// link
+import { Link, useFetcher } from "react-router-dom";
+// useFetch hook
+import useFetch from "../hooks/useFetch";
 
 const CategoryNavMobile = ({ setCatnavMobile }) => {
+  // get categories
+  const { data } = useFetch("/categories");
+
   return (
     <div className="w-full h-full bg-primary p-8">
       {/* close icon */}
@@ -11,6 +18,19 @@ const CategoryNavMobile = ({ setCatnavMobile }) => {
         className="flex justify-end mb-8 cursor-pointer"
       >
         <FiX className="text-3xl" />
+      </div>
+      <div className="flex flex-col gap-y-8">
+        {data?.map((category) => {
+          return (
+            <Link
+              to={`products/${category.id}`}
+              className="uppercase font-medium"
+              key={category.id}
+            >
+              {category.attributes.title} Cameras
+            </Link>
+          );
+        })}
       </div>
       CategoryNavMobile
     </div>
