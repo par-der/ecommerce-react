dc12aae23e979f3e54111e43e40b875a0340203f
par-der <egorterentev58@gmail.com>
2023-06-01 11:14:57 +0400

    add final product details page

--- client/src/components/RelatedProducts.js
+++ client/src/components/RelatedProducts.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+// useFetch hook
+import useFetch from "../hooks/useFetch";
+// components
+import ProductSlider from "./ProductSlider";
 
-const RelatedProducts = () => {
-  return <div>RelatedProducts</div>;
+const RelatedProducts = ({ categoryTitle }) => {
+  // get products by category title
+  const { data } = useFetch(
+    `/products?populate=*&filters[categories][title]=${categoryTitle}`
+  );
+
+  return (
+    <div className="mb-16">
+      <div className="container mx-auto">
+        <h2>Related Products</h2>
+        <ProductSlider data={data} />
+      </div>
+    </div>
+  );
 };
 
 export default RelatedProducts;
--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -16,6 +16,8 @@ const ProductDetails = () => {
   if (!data) {
     return <div className="container mx-auto">loading...</div>;
   }
+  // category title
+  const categoryTitle = data[0].attributes.categories.data[0].attributes.title;
   return (
     <div className="mb-16 pt-44 lg:pt-[30px] xl:pt-0">
       <div className="container mx-auto">
@@ -41,7 +43,7 @@ const ProductDetails = () => {
           </div>
         </div>
         {/* related products */}
-        <RelatedProducts />
+        <RelatedProducts categoryTitle={categoryTitle} />
       </div>
     </div>
   );
