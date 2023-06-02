64595df980e3a2291bc51f7fc25aec6e9666ae4f
par-der <egorterentev58@gmail.com>
2023-06-02 11:37:47 +0400

    finish cart

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -33,9 +33,9 @@ const CartItem = ({ item }) => {
           {/* quantity */}
           <div className="flex gap-x-4 mb-2">
             <Qty item={item} />
-            <div className="text-accent text-xl">
-              $ {item.attributes.price * item.amount}
-            </div>
+          </div>
+          <div className="text-accent text-xl">
+            $ {item.attributes.price * item.amount}
           </div>
         </div>
         {/* price */}
--- client/src/context/CartContext.js
+++ client/src/context/CartContext.js
@@ -18,6 +18,14 @@ const CartProvider = ({ children }) => {
     setItemsAmount(amount);
   }, [cart]);
 
+  // cart total
+  useEffect(() => {
+    const total = cart.reduce((a, c) => {
+      return a + c.attributes.price * c.amount;
+    }, 0);
+    setTotal(total);
+  }, [cart]);
+
   // add to cart
   const addToCart = (item, id) => {
     const itemID = parseInt(id);
@@ -53,6 +61,57 @@ const CartProvider = ({ children }) => {
     setCart(newCart);
   };
 
+  // handle input
+  const handleInput = (e, id) => {
+    const value = parseInt(e.target.value);
+    // find the item in the cart by id
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+
+    if (cartItem) {
+      const newCart = cart.map((item) => {
+        if (item.id === id) {
+          if (isNaN(value)) {
+            setAmount(1);
+            return { ...item, amount: 1 };
+          } else {
+            setAmount(value);
+            return { ...item, amount: value };
+          }
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    }
+    setIsOpen(true);
+  };
+
+  // handle select
+  const handleSelect = (e, id) => {
+    const value = parseInt(e.target.value);
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    if (cartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          setAmount(value);
+          return { ...item, amount: value };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    }
+  };
+
+  // clear cart
+  const clearCart = () => {
+    setCart([]);
+  };
+
   return (
     <CartContext.Provider
       value={{
@@ -62,6 +121,10 @@ const CartProvider = ({ children }) => {
         cart,
         removeFromCart,
         itemsAmount,
+        handleInput,
+        handleSelect,
+        total,
+        clearCart,
       }}
     >
       {children}
